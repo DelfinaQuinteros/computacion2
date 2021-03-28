883277c821daa1dcf993c6b5d372c49016ce7e10
DelfinaQuinteros <65193862+DelfinaQuinteros@users.noreply.github.com>
2021-03-28 15:12:37 -0300

    EJERCICIO 1

--- getopt/calculadora.py
+++ getopt/calculadora.py
@@ -5,21 +5,26 @@ import getopt
 
 (opt, arg) = getopt.getopt(sys.argv[1:], 'o:n:m:')
 
-print("opciones: ", opt)
 
 for (op, ar) in opt:
     if op in '-o':
-        print("Opcion -o seteada: ", ar)
         operador = ar
     elif op == '-n':
-        print("Opcion -n seteada: ", ar)
         num1 = ar
     elif op == '-m':
-        print("Opcion -m seteada: ", ar)
         num2 = ar
     else:
         print("Opcion invalida")
 
-
+if operador == '+':
+    print("El resultado de la suma de:", num1, "+", num2, "es:", int(num1) + int(num2))
+elif operador == '-':
+    print("El resultado de la resta de:", num1, "-", num2, "es:", int(num1) - int(num2))
+elif operador == '*':
+    print("El resultado de la multiplicacion de:", num1, "*", num2, "es:", int(num1) * int(num2))
+elif operador == '/':
+    print("El resultado de la division de:", num1, "/", num2, "es:", int(num1) / int(num2))
+else:
+    print("Ingrese una opcion valida")
 
 
