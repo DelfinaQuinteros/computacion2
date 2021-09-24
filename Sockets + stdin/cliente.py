ffc8f2b236c33136db49e2af9a5c5d0b3a2f1809
delfina quinteros <dquinteros4297@gmail.com>
2021-09-24 12:09:50 -0300

    tcp_udp

--- Sockets + stdin/cliente.py
+++ Sockets + stdin/cliente.py
@@ -1,66 +1,56 @@
 #!/usr/bin/python3
 import getopt
 import socket
+import sys
 from sys import argv, exit
 
+try:
+    (opt, arg) = getopt.getopt(sys.argv[1:], 'a:p:t:', [])
+except getopt.GetoptError as err:
+    print(err)
+    sys.exit(1)
 
-def get_Op():
+for (op, ar) in opt:
+    if op in ['-a']:
+        host = ar
+    elif op in ['-p']:
+        port = int(ar)
+    elif op in ['-t']:
+        protocol = ar
+    else:
+        sys.exit(2)
+
+if protocol == 'tcp' or protocol == 'TPC':
+    print("Protocolo TCP seleccionado")
     try:
-        (opcion, args) = getopt.getopt(argv[1:], 'a:t:p', [])
-        return opcion
-    except getopt.GetoptError as error:
-        print("Error:", str(error))
+        ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error:
+        print("No se pudo crear el socket")
         exit()
-
-
-def socketStructure(host, port, protocol):
-    host = host
-    protocol = protocol
-    port = port
-    if protocol == 'tcp' or protocol == 'TPC':
-        print("Protocolo TCP seleccionado")
+    ss.connect((host, port))
+    while True:
         try:
-            ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        except socket.error:
-            print("No se pudo crear el socket")
-        exit()
-        ss.connect((host, port))
-        while True:
-            try:
-                msgin = input('Ingresar mensaje: ')
-                ss.send(msgin.encode('ascii'))
-                if msgin == 'exit' or msgin == 'EXIT':
-                    ss.close()
-                    break
-            except EOFError:
-                break
-    elif protocol == 'udp' or protocol == 'UDP':
-        print("Protocolo UDP seleccionado")
-        try:
-            ss = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        except socket.error:
-            print('No se pudo crear el socket')
-            exit()
-        while True:
-            msgin = input("Ingresar mensaje:").encode()
-            ss.sendto(msgin, (host, port))
-            if msgin.decode() == 'exit':
+            msgin = input('Ingresar mensaje: ')
+            ss.send(msgin.encode('ascii'))
+            if msgin == 'exit' or msgin == 'EXIT':
                 ss.close()
                 break
+        except EOFError:
+            break
+elif protocol == 'udp' or protocol == 'UDP':
+    print("Protocolo UDP seleccionado")
+    try:
+        ss = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    except socket.error:
+        print('No se pudo crear el socket')
+        exit()
+    while True:
+        msgin = input("Ingresar mensaje:").encode()
+        ss.sendto(msgin, (host, port))
+        if msgin.decode() == 'exit':
+            ss.close()
+            break
     else:
         print("No se ha podido reconocer el protocolo, por favor ingrese 'UDP' o 'TCP'")
 
 
-def main():
-    options = get_Op()
-    for (opts, args) in options:
-        if opts == '-p':
-            port = args
-        if opts == '-t':
-            protocol = args
-        if opts == '-a':
-            host = args
-    socketStructure(host, port, protocol)
-
-
-main()
--- Sockets + stdin/servidor.py
+++ Sockets + stdin/servidor.py
@@ -1,77 +1,64 @@
+#!/usr/bin/python3
 import getopt
 import socket
 import sys
 
+try:
+    (opt, arg) = getopt.getopt(sys.argv[1:], 'p:t:f:')
+except getopt.GetoptError as err:
+    print(err)
+    sys.exit(2)
 
-def main():
-    def get_op():
-        try:
-            (opcion, arg) = getopt.getopt(sys.argv[1:], "p:t:f")
-            return opcion
-        except getopt.GetoptError as error:
-            print("Error: ", str(error))
-            exit()
+for (op, ar) in opt:
+    if op in ['-p']:
+        port = int(ar)
+    elif op in ['-t']:
+        protocol = ar
+    elif op in ['-f']:
+        pathfile = ar
+    else:
+        sys.exit(2)
 
-    opciones = get_op()
-    for (opcion, arg) in opciones:
-        if opcion == '-p':
-            port = arg
-        if opcion == '-t':
-            protocol = arg
-        if opcion == '-f':
-            pathfile = arg
+if protocol == 'tcp' or protocol == 'TCP':
+    print("Protocolo TCP seleccionado")
+    ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = ""
+    ss.bind((host, port))
+    ss.listen(5)
+    print("-----Server listening-----")
+    clientesocket, addr = ss.accept()
 
-    def socket_structure(port, protocol, pathfile):
-        port = port
-        protocol = protocol
-        file = pathfile
+    while True:
+        f = open(pathfile, "a")
+        d = clientesocket.recv(1024)
+        f.write(d.decode("ascii") + "\n")
+        if d == "" or len(d) == 0:
+            print("Saliendo")
+            break
+        print("Direccion: %s" % str(addr))
+        print("Recibido correctamente:" + d.decode('ascii'))
 
-        if protocol == 'tcp' or protocol == 'TCP':
-            print("Protocolo TCP seleccionado")
-            ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            host = ""
-            ss.bind((host, port))
-            ss.listen(5)
-            print("-----Server listening-----")
-            clientesocket, addr = ss.accept()
+elif protocol == 'udp' or protocol == 'UDP':
+    print("Protocolo UDP seleccionado")
+    ss = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    host = ""
+    ss.bind((host, port))
+    print("-----Server listening-----")
 
-            while True:
-                f = open(file, "a")
-                d = clientesocket.recv(1024)
-                f.write(d.decode("ascii") + "\n")
-                if d == "" or len(d) == 0:
-                    print("Saliendo")
-                    break
-                print("Direccion: %s" % str(addr))
-                print("Recibido correctamente:" + d.decode('ascii'))
+    while True:
+        f = open(pathfile, "a")
+        d, addr = ss.recvfrom(1024)
+        msg = d.decode("ascii")
+        f.write(msg + "\n")
+        address = addr[0]
+        port = addr[1]
+        if d == "" or len(d) == 0:
+            print("Exit")
+            break
+        print("Direccion: %s - Port %d" % (address, port))
+        print("Recibido correctamente:" + d.decode('ascii'))
 
-        elif protocol == 'udp' or protocol == 'UDP':
-            print("Protocolo UDP seleccionado")
-            ss = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            host = ""
-            ss.bind((host, port))
-            print("-----Server listening-----")
-
-            while True:
-                f = open(file, "a")
-                d, addr = ss.recvfrom(1024)
-                f.write(d.decode("ascii") + "\n")
-                address = addr[0]
-                port = addr[1]
-
-                if d == "" or len(d) == 0:
-                    print("Saliendo")
-                    break
-                print("Direccion: %s - Port %d" % (address, port))
-                print("Recibido correctamente:" + d.decode('ascii'))
-
-        else:
-            print("No se ha podido reconocer el protocolo, por favor ingrese 'UDP' o 'TCP'")
-
-    socket_structure(port, protocol, pathfile)
-
-
-main()
-
-
-
+    else:
+        print("No se ha podido reconocer el protocolo, por favor ingrese 'UDP' o 'TCP'")
+else:
+    sys.exit()
