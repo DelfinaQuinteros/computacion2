c863e33a7d923effd403e79a8668d3570224ead6
delfina quinteros <dquinteros4297@gmail.com>
2021-11-03 11:02:24 -0300

    asyncio

--- asyncio/cliente.py
+++ asyncio/cliente.py
@@ -3,37 +3,37 @@ import datetime
 import getopt
 import socket
 import sys
+import asyncio
 
 
-def main():
+async def main():
     (opcion, arg) = getopt.getopt(sys.argv[1:], "l:")
     try:
-        ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        reader, writer = await asyncio.open_connection('127.0.0.1', 8888)
     except socket.error:
         print("No se ha podido crear el socket")
         sys.exit()
-    host = "0.0.0.0"
-    port = 8000
-    ss.connect((host, port))
     print("----------Comando prompt----------")
     comando = ""
 
     while comando != 'exit':
         comando = input("Comando:")
         msg = comando
-        ss.send(msg.encode('ascii'))
-        msg = ss.recv(1024)
-        print("Respuesta del servidor: " + msg.decode())
+        writer.write(msg.encode('ascii'))
+        await writer.drain()
+        data = await reader.read(10)
+        print(f'Received: {data.decode()!r}')
+        await writer.wait_closed()
 
         for (opt, arg) in opcion:
             if opt == "-l":
                 file_path = arg
                 file = open(str(file_path), "a")
                 datetime_today = datetime.datetime.today()
-                file.writelines("\n" + str(datetime_today) + "\n" + msg.decode())
+                file.writelines("\n" + str(datetime_today) + "\n" + msg.decode('ascii'))
                 file.close()
 
     exit()
 
 
-main()
+asyncio.run(main())
--- asyncio/servidor.py
+++ asyncio/servidor.py
@@ -3,38 +3,36 @@ import subprocess, socket
 import asyncio
 import socket
 
-loop = asyncio.get_event_loop()
 
-
-async def handle_client(cliente, addr):
+async def servidor(reader, writer):
+    addr = writer.get_extra_info('peername')
     print(f"Cliente conectado: {addr}")
-    while True:
-        data = clientesocket.recv(1024)
-        msg = data.decode("ascii")
-        if data.decode('ascii') == 'exit':
-            clientesocket.send('Terminado'.encode('ascii'))
-            break
-        print("Direccion: %s" % str(addr))
-        print("Recibido correctamente:" + data.decode('ascii'))
-        resultado = subprocess.Popen([data], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        stdout, stderr = resultado.communicate()
-        if stdout != "":
-            msg = "OK\n" + stdout
-        elif stderr != "":
-            msg = "ERROR\n" + stderr
+    data = await reader.read(10)
+    message = data.decode('ascii')
+    if data.decode('ascii') == 'exit':
+        writer.write('Terminado'.encode('ascii'))
+        writer.close()
+    print("Direccion: %s" % str(addr))
+    print("Recibido correctamente:" + data.decode('ascii'))
+    resultado = subprocess.Popen([data], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    stdout, stderr = resultado.communicate()
+    if stdout != "":
+        msg = "OK\n" + stdout
+    elif stderr != "":
+        msg = "ERROR\n" + stderr
+        writer.write(message.encode("ascii"))
+    await writer.drain()
 
-        clientesocket.send(msg.encode("ascii"))
+
+async def main():
+    server = await asyncio.start_server(
+        servidor, '127.0.0.1', 8888)
+    addr = server.sockets[0].getsockname()
+    async with server:
+        await server.serve_forever()
 
 
 if __name__ == "__main__":
-    ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = ""
-    port = 8000
-    ss.bind((host, port))
-    ss.listen(5)
-    print("----------Server listening----------")
-
-    while True:
-        cliente = ss.accept()
-        clientesocket, addr = cliente
+    print("-------------servidor escuchando------------------")
+    asyncio.run(main())
 
